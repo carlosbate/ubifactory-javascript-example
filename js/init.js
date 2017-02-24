7c7c66c3401fde281af3cd281dbc0e70e4c49289
Carlos Rijo <c.bate@campus.fct.unl.pt>
2017-02-24 02:42:14 +0000

    init.js changes on host, port and url

--- js/init.js
+++ js/init.js
@@ -1,207 +1,207 @@
 (function($){
-  $(function() {
+    $(function() {
 
-      const HOST = 'localhost';
-      const PORT = '8989';
-      const URL = 'http://' + HOST + ':' + PORT;
+        const HOST = 'localhost';
+        const PORT = '8989';
+        const URL = 'http://' + HOST + ':' + PORT;
 
-      var ubiId, redraw = false;
+        var ubiId, redraw = false;
 
-      transpose = function (a) {
+        transpose = function (a) {
 
-          // Calculate the width and height of the Array
-          var w = a.length ? a.length : 0,
-              h = a[0] instanceof Array ? a[0].length : 0;
+            // Calculate the width and height of the Array
+            var w = a.length ? a.length : 0,
+                h = a[0] instanceof Array ? a[0].length : 0;
 
-          // In case it is a zero matrix, no transpose routine needed.
-          if (h === 0 || w === 0) {
-              return [];
-          }
+            // In case it is a zero matrix, no transpose routine needed.
+            if (h === 0 || w === 0) {
+                return [];
+            }
 
-          /**
-           * @var {Number} i Counter
-           * @var {Number} j Counter
-           * @var {Array} t Transposed data is stored in this array.
-           */
-          var i, j, t = [];
+            /**
+             * @var {Number} i Counter
+             * @var {Number} j Counter
+             * @var {Array} t Transposed data is stored in this array.
+             */
+            var i, j, t = [];
 
-          // Loop through every item in the outer array (height)
-          for (i = 0; i < h; i++) {
+            // Loop through every item in the outer array (height)
+            for (i = 0; i < h; i++) {
 
-              // Insert a new row (array)
-              t[i] = [];
+                // Insert a new row (array)
+                t[i] = [];
 
-              // Loop through every item per item in outer array (width)
-              for (j = 0; j < w; j++) {
+                // Loop through every item per item in outer array (width)
+                for (j = 0; j < w; j++) {
 
-                  // Save transposed data.
-                  t[i][j] = a[j][i];
-              }
-          }
+                    // Save transposed data.
+                    t[i][j] = a[j][i];
+                }
+            }
 
-          return t;
-      };
+            return t;
+        };
 
-      populateUbiInfo = function(){
-          $('#ubi-info').html('<div class="row" id="cp-heatmaps"> <h3 class="center">Component Planes</h3> </div> <div class="row" id="hit-umat-heatmap"> <h3 class="center">Hit-Histogram & U-Mat</h3> </div> <div class="row" id="prod-div-heatmap"> <h3 class="center">Product & Division</h3> </div>');
-      };
+        populateUbiInfo = function(){
+            $('#ubi-info').html('<div class="row" id="cp-heatmaps"> <h3 class="center">Component Planes</h3> </div> <div class="row" id="hit-umat-heatmap"> <h3 class="center">Hit-Histogram & U-Mat</h3> </div> <div class="row" id="prod-div-heatmap"> <h3 class="center">Product & Division</h3> </div>');
+        };
 
-      loadingAnimation = function(){
-          $('#ubi-info').html('<div class="center" style="margin-top: 25%;"><div class="preloader-wrapper big active"> <div class="spinner-layer spinner-blue"> <div class="circle-clipper left"> <div class="circle"></div> </div><div class="gap-patch"> <div class="circle"></div> </div><div class="circle-clipper right"> <div class="circle"></div> </div> </div> <div class="spinner-layer spinner-red"> <div class="circle-clipper left"> <div class="circle"></div> </div><div class="gap-patch"> <div class="circle"></div> </div><div class="circle-clipper right"> <div class="circle"></div> </div> </div> <div class="spinner-layer spinner-yellow"> <div class="circle-clipper left"> <div class="circle"></div> </div><div class="gap-patch"> <div class="circle"></div> </div><div class="circle-clipper right"> <div class="circle"></div> </div> </div> <div class="spinner-layer spinner-green"> <div class="circle-clipper left"> <div class="circle"></div> </div><div class="gap-patch"> <div class="circle"></div> </div><div class="circle-clipper right"> <div class="circle"></div> </div> </div></div></div> ');
-      };
+        loadingAnimation = function(){
+            $('#ubi-info').html('<div class="center" style="margin-top: 25%;"><div class="preloader-wrapper big active"> <div class="spinner-layer spinner-blue"> <div class="circle-clipper left"> <div class="circle"></div> </div><div class="gap-patch"> <div class="circle"></div> </div><div class="circle-clipper right"> <div class="circle"></div> </div> </div> <div class="spinner-layer spinner-red"> <div class="circle-clipper left"> <div class="circle"></div> </div><div class="gap-patch"> <div class="circle"></div> </div><div class="circle-clipper right"> <div class="circle"></div> </div> </div> <div class="spinner-layer spinner-yellow"> <div class="circle-clipper left"> <div class="circle"></div> </div><div class="gap-patch"> <div class="circle"></div> </div><div class="circle-clipper right"> <div class="circle"></div> </div> </div> <div class="spinner-layer spinner-green"> <div class="circle-clipper left"> <div class="circle"></div> </div><div class="gap-patch"> <div class="circle"></div> </div><div class="circle-clipper right"> <div class="circle"></div> </div> </div></div></div> ');
+        };
 
-      drawHeatmap = function (cp, title, elem) {
-          var values = [];
-          for (i = 0; i < cp.length; i += 20) {
-              var res = [];
-              for (k = 0; k < 20; k++)
-                  res.push(cp[i + k]);
-              values.push(res);
-          }
-          var layout = {
-              title: title,
-              xaxis: {fixedrange: true},
-              yaxis: {fixedrange: true},
-              showlegend: false
-          };
-          var data = [
-              {
-                  z: transpose(values),
-                  type: 'heatmap'
-              }
-          ];
+        drawHeatmap = function (cp, title, elem) {
+            var values = [];
+            for (i = 0; i < cp.length; i += 20) {
+                var res = [];
+                for (k = 0; k < 20; k++)
+                    res.push(cp[i + k]);
+                values.push(res);
+            }
+            var layout = {
+                title: title,
+                xaxis: {fixedrange: true},
+                yaxis: {fixedrange: true},
+                showlegend: false
+            };
+            var data = [
+                {
+                    z: transpose(values),
+                    type: 'heatmap'
+                }
+            ];
 
-          var myPlot = document.getElementById(elem);
+            var myPlot = document.getElementById(elem);
 
-          if(!redraw){
-              Plotly.plot(elem, data, layout, {displayModeBar: false});
+            if(!redraw){
+                Plotly.plot(elem, data, layout, {displayModeBar: false});
 
-              myPlot.on('plotly_click', function (data) {
-                  var infotext = data.points.map(function (d) {
-                      console.log(d);
-                      return ('x= ' + d.x + ' , y= ' + d.y + ', value=' + d.z);
-                  });
-                  console.log(infotext);
-              });
-          }
-          else
-              Plotly.newPlot(elem, data, layout, {displayModeBar: false});
-      };
+                myPlot.on('plotly_click', function (data) {
+                    var infotext = data.points.map(function (d) {
+                        console.log(d);
+                        return ('x= ' + d.x + ' , y= ' + d.y + ', value=' + d.z);
+                    });
+                    console.log(infotext);
+                });
+            }
+            else
+                Plotly.newPlot(elem, data, layout, {displayModeBar: false});
+        };
 
-      var newUbi = {
-          "width": 40,
-          "height": 20,
-          "dim": 2,
-          "alpha_i": 0.1,
-          "alpha_f": 0.08,
-          "sigma_i": 0.6,
-          "sigma_f": 0.2,
-          "beta_value": 0.7,
-          "normalization": {"type": "NONE"}
-      };
+        var newUbi = {
+            "width": 40,
+            "height": 20,
+            "dim": 2,
+            "alpha_i": 0.1,
+            "alpha_f": 0.08,
+            "sigma_i": 0.6,
+            "sigma_f": 0.2,
+            "beta_value": 0.7,
+            "normalization": {"type": "NONE"}
+        };
 
-      createUbi = function () {
-          $.post(URL + '/ubis', JSON.stringify(newUbi))
-              .done(function (data) {
-                  ubiId = data.id;
-                  loadingAnimation();
-                  getUbiData(ubiId);
-              });
-      }
+        createUbi = function () {
+            $.post(URL + '/ubis', JSON.stringify(newUbi))
+                .done(function (data) {
+                    ubiId = data.id;
+                    loadingAnimation();
+                    getUbiData(ubiId);
+                });
+        }
 
-      deleteUbi = function() {
-          $.ajax({
-              url: URL + '/ubis/' + ubiId,
-              type: 'DELETE',
-              success: function(result) {
-                  $('#ubi-info').html('<h4>Create a new UbiSOM</h4>');
-              }
-          });
-      }
+        deleteUbi = function() {
+            $.ajax({
+                url: URL + '/ubis/' + ubiId,
+                type: 'DELETE',
+                success: function(result) {
+                    $('#ubi-info').html('<h4>Create a new UbiSOM</h4>');
+                }
+            });
+        }
 
-      getUbiId = function(){
-          console.log(ubiId)
-      };
+        getUbiId = function(){
+            console.log(ubiId)
+        };
 
-      patchUbiSOM = function (id) {
-          $.getJSON('data/data_quad.json', function(json) {
-              $.ajax({
-                  url: URL + '/ubis/' + id,
-                  type: 'PATCH',
-                  processData: false,
-                  contentType : 'application/json',
-                  dataType: 'json',
-                  error: function (e) {
-                      if(e.status == 202){
-                          Materialize.toast('Give me more!', 2500) // 4000 is the duration of the toast
-                          console.log("feed ok");
-                      }
-                      else
-                          console.log("patch error: " + e);
-                  },
-                  data: JSON.stringify(json)
-              });
-          });
-      };
+        patchUbiSOM = function (id) {
+            $.getJSON('data/data_quad.json', function(json) {
+                $.ajax({
+                    url: URL + '/ubis/' + id,
+                    type: 'PATCH',
+                    processData: false,
+                    contentType : 'application/json',
+                    dataType: 'json',
+                    error: function (e) {
+                        if(e.status == 202){
+                            Materialize.toast('Give me more!', 2500) // 4000 is the duration of the toast
+                            console.log("feed ok");
+                        }
+                        else
+                            console.log("patch error: " + e);
+                    },
+                    data: JSON.stringify(json)
+                });
+            });
+        };
 
-      feed = function() {
-        patchUbiSOM(ubiId);
-      };
+        feed = function() {
+            patchUbiSOM(ubiId);
+        };
 
 
-      getUbiData = function(id) {
-          $.getJSON(URL + '/ubis/' + id + '/data', function(json) {
+        getUbiData = function(id) {
+            $.getJSON(URL + '/ubis/' + id + '/data', function(json) {
 
-              populateUbiInfo();
+                populateUbiInfo();
 
-              var quad_data = json;
+                var quad_data = json;
 
-              var nrFeatures = quad_data.data[0].weights.length;
-              var componentPlanes = [[]];
-              var componentPlanesLabels = ['X','Y'];
+                var nrFeatures = quad_data.data[0].weights.length;
+                var componentPlanes = [[]];
+                var componentPlanesLabels = ['X','Y'];
 
-              var hit = quad_data.data.map(function (d){ return d.hit_count});
-              var umat = quad_data.data.map(function (d){ return d.distance});
+                var hit = quad_data.data.map(function (d){ return d.hit_count});
+                var umat = quad_data.data.map(function (d){ return d.distance});
 
-              var prod = quad_data.data.map(function(d){return d.distance * (0.1 + d.hit_count)});
-              var div = quad_data.data.map(function(d){return d.distance / (0.1 + d.hit_count)});
+                var prod = quad_data.data.map(function(d){return d.distance * (0.1 + d.hit_count)});
+                var div = quad_data.data.map(function(d){return d.distance / (0.1 + d.hit_count)});
 
-              for(i = 0; i < nrFeatures; i++)
-                  componentPlanes[i] = quad_data.data.map(function(d){ return d.weights[i]});
+                for(i = 0; i < nrFeatures; i++)
+                    componentPlanes[i] = quad_data.data.map(function(d){ return d.weights[i]});
 
-              /** Draw Component Planes **/
+                /** Draw Component Planes **/
 
-              componentPlanes.forEach(function(cp,i){
-                  $('#cp-heatmaps').append('<div class="col s12 m12 l6"><div id="cp-heatmap-' + i + '"></div></div>');
-                  drawHeatmap(cp,componentPlanesLabels[i],'cp-heatmap-' + i);
-              });
+                componentPlanes.forEach(function(cp,i){
+                    $('#cp-heatmaps').append('<div class="col s12 m12 l6"><div id="cp-heatmap-' + i + '"></div></div>');
+                    drawHeatmap(cp,componentPlanesLabels[i],'cp-heatmap-' + i);
+                });
 
-              /** Draw Hit-Histogram **/
-              $('#hit-umat-heatmap').append('<div class="col s12 m12 l6"><div id="hit-histogram-heatmap"></div></div>');
-              drawHeatmap(hit, 'Hit-Histogram', 'hit-histogram-heatmap');
+                /** Draw Hit-Histogram **/
+                $('#hit-umat-heatmap').append('<div class="col s12 m12 l6"><div id="hit-histogram-heatmap"></div></div>');
+                drawHeatmap(hit, 'Hit-Histogram', 'hit-histogram-heatmap');
 
-              /** Draw UMat **/
-              $('#hit-umat-heatmap').append('<div class="col s12 m12 l6"><div id="umat-heatmap"></div></div>');
-              drawHeatmap(umat, 'U-Matrix', 'umat-heatmap');
+                /** Draw UMat **/
+                $('#hit-umat-heatmap').append('<div class="col s12 m12 l6"><div id="umat-heatmap"></div></div>');
+                drawHeatmap(umat, 'U-Matrix', 'umat-heatmap');
 
-              /** Draw Prod **/
-              $('#prod-div-heatmap').append('<div class="col s12 m12 l6"><div id="prod-heatmap"></div></div>');
-              drawHeatmap(prod, 'U-Mat * Hit', 'prod-heatmap');
+                /** Draw Prod **/
+                $('#prod-div-heatmap').append('<div class="col s12 m12 l6"><div id="prod-heatmap"></div></div>');
+                drawHeatmap(prod, 'U-Mat * Hit', 'prod-heatmap');
 
-              /** Draw Div **/
-              $('#prod-div-heatmap').append('<div class="col s12 m12 l6"><div id="div-heatmap"></div></div>');
-              drawHeatmap(div, 'U-Mat / Hit', 'div-heatmap');
+                /** Draw Div **/
+                $('#prod-div-heatmap').append('<div class="col s12 m12 l6"><div id="div-heatmap"></div></div>');
+                drawHeatmap(div, 'U-Mat / Hit', 'div-heatmap');
 
-              console.log("refresh");
+                console.log("refresh");
 
-          });
-      };
+            });
+        };
 
-      updateGraph = function(){
-          redraw = true;
-          loadingAnimation();
-          getUbiData(ubiId);
-      };
+        updateGraph = function(){
+            redraw = true;
+            loadingAnimation();
+            getUbiData(ubiId);
+        };
 
-  }); // end of document ready
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
